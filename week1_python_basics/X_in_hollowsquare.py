3192c23a507fa01e8678ed6b43205b66e6f99d37
Sunny Singh <sunny762902@gmail.com>
2025-06-27 13:59:16 +0530

    Update X_in_hollowsquare.py

--- week1_python_basics/X_in_hollowsquare.py
+++ week1_python_basics/X_in_hollowsquare.py
@@ -7,6 +7,8 @@ def x_in_hollow_square(n):
                 print("*", end=" ")
             else:
                 print(" ", end=" ")
+            
+        print()
 
 lines = int(input("Enter number of lines for X in Hollow Square: "))
 x_in_hollow_square(lines)
