4b2272c9cab37305630c83a47564fe894939a916
13SunnySingh12 <sunny762902@gmail.com>
2025-07-02 11:37:10 +0530

    Removed week1_python_basics from GitHub but kept locally

--- week1_python_basics/math_table.py
+++
@@ -1,6 +0,0 @@
-#Accept a number from thr user and print it's math table.
-
-input_number = int(input("Enter a number to print its Math table: "))
-
-for i in range(1,11):  
-    print('%2d * %2d = %3d' % (input_number, i, input_number * i))
--- week1_python_basics/p4.py
+++
@@ -1,7 +0,0 @@
-# Check if a year is Leap year
-input_year = int(input("Enter a year: "))
-
-if (input_year % 4 == 0 and input_year % 100 != 0) or (input_year % 400 == 0):
-    print(input_year," is a leap year.")
-else:
-    print(input_year," is not a leap year.")
