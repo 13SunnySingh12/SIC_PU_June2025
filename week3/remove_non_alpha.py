321182405cba1541aed7373f3e128a40ec0a5f8a
13SunnySingh12 <sunny762902@gmail.com>
2025-07-11 09:18:45 +0530

    Added week3 Python work

---
+++ week3/remove_non_alpha.py
@@ -0,0 +1,8 @@
+import re
+
+data = ["Hello123", "A@B#C", "Clean_text", "X Y Z"]
+
+# Remove all non-alphabetic characters
+cleaned = list(map(lambda s: re.sub(r'[^a-zA-Z]', '', s), data))
+print(cleaned) 
+                       
